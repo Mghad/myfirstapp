1d61be51391033d86e96c306c20cd2fec527d401
Mghad <mitch.hadfield1@gmail.com>
2019-03-07 17:43:07 -0500

    new commit

---
+++ tethysapp/myfirstapp/app.py
@@ -0,0 +1,34 @@
+from tethys_sdk.base import TethysAppBase, url_map_maker
+
+
+class Myfirstapp(TethysAppBase):
+    """
+    Tethys app class for Myfirstapp.
+    """
+
+    name = 'Myfirstapp'
+    index = 'myfirstapp:home'
+    icon = 'myfirstapp/images/icon.gif'
+    package = 'myfirstapp'
+    root_url = 'myfirstapp'
+    color = '#c0392b'
+    description = 'Place a brief description of your app here.'
+    tags = ''
+    enable_feedback = False
+    feedback_emails = []
+
+    def url_maps(self):
+        """
+        Add controllers
+        """
+        UrlMap = url_map_maker(self.root_url)
+
+        url_maps = (
+            UrlMap(
+                name='home',
+                url='myfirstapp',
+                controller='myfirstapp.controllers.home'
+            ),
+        )
+
+        return url_maps
